56b211b900f1d1f91ad135c563f849e48726f5c5
FND <fnd@fnd-osmosoft.(none)>
2009-07-17 17:32:48 +0100

    added utility functions to add bags and recipes

--- js/configurator.js
+++ js/configurator.js
@@ -1,46 +1,34 @@
 // mainly nicked from WiringEditor
 function getValue(layer) {
-    
    var i;
    var obj = {modules: [], wires: []};
-
    for (i=0; i<layer.containers.length; i++) {
       obj.modules.push( {name: layer.containers[i].options.title, value: layer.containers[i].getValue(), config: layer.containers[i].getConfig()});
    }
-
    for(i=0; i<layer.wires.length; i++) {
       var wire = layer.wires[i];
-      var wireObj = { 
-         src: {moduleId: WireIt.indexOf(wire.terminal1.container, layer.containers), terminal: wire.terminal1.options.name}, 
+      var wireObj = {
+         src: {moduleId: WireIt.indexOf(wire.terminal1.container, layer.containers), terminal: wire.terminal1.options.name},
          tgt: {moduleId: WireIt.indexOf(wire.terminal2.container, layer.containers), terminal: wire.terminal2.options.name}
       };
       obj.wires.push(wireObj);
    }
-
    return {
       working: obj
    };
 }
 
-var layer = null;
-YAHOO.util.Event.onDOMReady( function() {
-	var canvas = document.getElementById('canvas');
-	layer = new WireIt.Layer({parentEl:canvas});
-	layer.addContainer({
-		xtype: "WireIt.InOutContainer",
-		inputs: ["text1", "text2", "option1"], 
-		outputs: ["result", "error"],
-		title: "test",
-		position: [0, 0],
-		
-	});
-	layer.addContainer({
-		xtype: "WireIt.InOutContainer",
-		inputs: ["text1", "text2", "option1"], 
-		outputs: ["result", "error"],
-		title: "test2",
-		position: [100, 100]
-	});
+YAHOO.util.Event.onDOMReady(function() {
+	try { // XXX: DEBUG
+		var canvas = document.getElementById("canvas"); // TODO: use YUI selector?
+		var layer = new WireIt.Layer({ parentEl: canvas });
+		addBag({ name: "Foo" }, layer);
+		addBag({ name: "Bar" }, layer);
+		addRecipe({ name: "Baz" }, layer);
+		layer.initContainers();
+	} catch(e) {
+		console.log(e);
+	}
 });
 
 function configure() {
@@ -83,4 +71,43 @@ function addExtraTerminal(container,name,type) {
 	});
 	container.bodyEl.appendChild(WireIt.cn('div', null, {lineHeight: "30px", textAlign: align}, name));
 	container.addTerminal(container.options.terminals[container.options.terminals.length-1]);
+}
+
+function addBag(bag, layer) {
+	bag = {
+		type: "bag",
+		name: bag.name,
+		pos_x: bag.pos_x,
+		pos_y: bag.pos_y
+	}; // TODO: merge recipe object into { type: "bag" }
+	return addEntity(bag, layer);
+}
+
+function addRecipe(recipe, layer) {
+	recipe = {
+		type: "recipe",
+		name: recipe.name,
+		pos_x: recipe.pos_x,
+		pos_y: recipe.pos_y
+	}; // TODO: merge recipe object into { type: "recipe" }
+	return addEntity(recipe, layer);
+}
+
+function addEntity(obj, layer) {
+	var x = obj.pos_x || 0;
+	var y = obj.pos_y || 0;
+	var container = {
+		xtype: "WireIt.InOutContainer",
+		inputs: ["write", "create", "delete", "manage", "accept"],
+		title: obj.name,
+		position: [x, y]
+	};
+	if(obj.type = "bag") {
+		container.outputs = ["tiddlers"];
+	}
+	container = layer.addContainer(container);
+	var el = container.el;
+	el.title = obj.desc || ""; // XXX: don't use title? -- XXX: use setAttribute?
+	YAHOO.util.Dom.addClass(el, obj.type);
+	return container;
 }
