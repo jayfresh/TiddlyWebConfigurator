2dd296f9367934642e51f73f0889c9fa2a6e7efd
FND <fnd@fnd-osmosoft.(none)>
2009-07-17 19:05:55 +0100

    added default positioning for bags and recipes

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ function init() {
 	try { // XXX: DEBUG
 		var canvas = document.getElementById("canvas"); // TODO: use YUI selector?
 		layer = new WireIt.Layer({ parentEl: canvas });
-		addBag({ name: "public" }, layer);
-		addBag({ name: "editors" }, layer);
-		addBag({ name: "system" }, layer);
-		addBag({ name: "team" }, layer);
-		addRecipe({ name: "website" }, layer);
-		addRecipe({ name: "CMS" }, layer);
-		addRecipe({ name: "wiki" }, layer);
+		addBag({ name: "public", pos_x: 10, pos_y: 0 }, layer);
+		addBag({ name: "editors", pos_x: 10, pos_y: 250 }, layer);
+		addBag({ name: "system", pos_x: 10, pos_y: 500 }, layer);
+		addBag({ name: "team", pos_x: 10, pos_y: 750 }, layer);
+		addRecipe({ name: "website", pos_x: 400, pos_y: 250 }, layer);
+		addRecipe({ name: "CMS", pos_x: 400, pos_y: 400 }, layer);
+		addRecipe({ name: "wiki", pos_x: 400, pos_y: 550 }, layer);
 		layer.initContainers();
 	} catch(e) {
 		console.log(e);
