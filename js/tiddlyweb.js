122fd90450c12e9d5aae355e2168fb0442334109
FND <fnd@fnd-osmosoft.(none)>
2009-07-18 17:55:49 +0100

    refactored AJAX functionality

--- js/tiddlyweb.js
+++ js/tiddlyweb.js
@@ -1,6 +1,6 @@
 var tiddlyweb;
 
-(function($) {
+(function() {
 
 var host = "http://localhost:8080"; // XXX: hardcoded
 
@@ -23,13 +23,7 @@ tiddlyweb = {
 		var data = {
 			policy: policy
 		};
-		$.localAjax({
-			url: uri,
-			type: "PUT",
-			dataType: "json",
-			data: YAHOO.lang.JSON.stringify(data),
-			complete: console.log // DEBUG
-		});
+		saveData(uri, data, console.log);
 	},
 
 	/*
@@ -44,27 +38,31 @@ tiddlyweb = {
 		for(var i = 0; i < bags.length; i++) {
 			data.recipe.push([bags[i], ""]);
 		}
-		$.localAjax({
-			url: uri,
-			type: "PUT",
-			dataType: "json",
-			data: YAHOO.lang.JSON.stringify(data),
-			complete: console.log // DEBUG
-		});
+		saveData(uri, data, console.log);
 	}
+};
+
+var saveData = function(uri, data, callback) {
+	localAjax({
+		url: uri,
+		type: "PUT",
+		dataType: "json",
+		data: YAHOO.lang.JSON.stringify(data),
+		complete: callback
+	});
 };
 
 /*
  * enable AJAX calls from a local file
- * triggers regular $.ajax call after requesting enhanced privileges
+ * triggers regular jQuery.ajax call after requesting enhanced privileges
  */
-$.localAjax = function(args) {
+var localAjax = function(args) {
 	if(document.location.protocol.indexOf("http") == -1 && window.Components &&
 		window.netscape && window.netscape.security) {
 		window.netscape.security.PrivilegeManager.
 			enablePrivilege("UniversalBrowserRead");
 	}
-	return $.ajax(args);
+	return jQuery.ajax(args); // TODO: use YUI
 };
 
-})(jQuery);
+})();
